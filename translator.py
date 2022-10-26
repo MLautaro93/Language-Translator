af53236e7eb6a93ca005d4461ec2877ab082fd7c
Lautaro <mlautaro93@gmail.com>
2022-10-26 10:48:09 -0300

    added text to speech converter

--- translator.py
+++ translator.py
@@ -1,7 +1,10 @@
-from tkinter import PhotoImage, ttk
+from tkinter import ttk
 import tkinter as tk
-from googletrans import Translator, LANGUAGES
+from googletrans import Translator, constants
+from gtts import gTTS
+from playsound import playsound
 from PIL import Image, ImageTk
+import os
 
 # Window
 root = tk.Tk()
@@ -22,7 +25,7 @@ translation_box = tk.Text(font = 'lucida 10', width = 25, height = 5, state = 'd
 translation_box.place(relx = .75, y = 100, anchor = tk.CENTER)
 
 # Combobox
-languages = [i.capitalize() for i in list(LANGUAGES.values())]
+languages = [i.capitalize() for i in list(constants.LANGCODES)]
 src_combobox = ttk.Combobox(values = languages, state = 'readonly')
 src_combobox.place(relx = .25, y = 200, anchor = tk.CENTER)
 dest_combobox = ttk.Combobox(values = languages, state = 'readonly')
@@ -32,11 +35,11 @@ dest_combobox.place(relx = .75, y = 200, anchor = tk.CENTER)
 def detect():
     text = text_box.get(1.0, tk.END)
     lang = translator.detect(text).lang
-    language = LANGUAGES[lang].capitalize()
+    language = constants.LANGUAGES[lang].capitalize()
     src_combobox.set(language)
 
 # Translate function
-def translate(*args):
+def translate():
     text = text_box.get(1.0, tk.END)
     translation_box.config(state = 'normal')
     translation_box.delete(1.0, tk.END)
@@ -61,6 +64,14 @@ def swap():
     src_combobox.set(y)
     dest_combobox.set(x)
 
+# Function to convert text to speech
+def convert(message, language):
+    lang = constants.LANGCODES[language.lower()]
+    speech = gTTS(message, lang = lang)
+    speech.save('speech.mp3')
+    playsound('speech.mp3')
+    os.remove('speech.mp3')
+
 # "Detect language" button
 detect_button = tk.Button(text = 'Detect language', command = detect)
 detect_button.place(relx = .25, y = 167, anchor = tk.CENTER)
@@ -68,12 +79,22 @@ detect_button.place(relx = .25, y = 167, anchor = tk.CENTER)
 # Translate button
 translate_button = tk.Button(text = 'Translate', fg = 'white', bg = 'dark blue', command = translate)
 translate_button.place(relx = .5, y = 200, anchor = tk.CENTER)
-root.bind('<Return>', translate)
 
 # Swap button
-img = Image.open('swap.png').resize((25, 25))
-image = ImageTk.PhotoImage(img)
-swap_button = tk.Button(image = image, command = swap)
+swap_img = Image.open('swap.png').resize((25, 25))
+swap_image = ImageTk.PhotoImage(swap_img)
+swap_button = tk.Button(image = swap_image, command = swap)
 swap_button.place(relx = .5, y = 100, anchor = tk.CENTER)
+
+# Audio buttons
+audio_img = Image.open('audio.jpg').resize((20, 20))
+audio_image = ImageTk.PhotoImage(audio_img)
+audio_button_1 = tk.Button(image = audio_image, 
+                           command = lambda: convert(text_box.get(1.0, tk.END), src_combobox.get()))
+audio_button_1.place(relx = .25, y = 40, anchor = tk.CENTER)
+audio_button_2 = tk.Button(image = audio_image, 
+                           command = lambda: convert(translation_box.get(1.0, tk.END), dest_combobox.get()))
+audio_button_2.place(relx = .75, y = 40, anchor = tk.CENTER)
+
 
 root.mainloop()
